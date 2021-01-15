32b44d10cd93e543f13d40cd75f86f38c247d42b
Erin Hanan <erin.hanan@wsu.edu>
2021-01-15 11:59:24 -0800

    cleaning up code

--- Rscripts/spatialOutMap.R
+++ Rscripts/spatialOutMap.R
@@ -1,19 +1,20 @@
 #title: "spatialOutMap"
 #author: "ErinJHanan"
 
-# read in patch daily results 
+# read in patch daily results, uses readin_rhessys_output function
 setwd("...")
-new50.p = readin_rhessys_output("new", p=1)
+new50.p = readin_rhessys_output("yourOutputPrefix", p=1)
 
 # subset individual days ... do for each day you output
-tmp = subset(new50.p$pdg, new50.p$pdg$year == 2001)
-new2001.10.pdg = subset(tmp, tmp$month == 10)
+tmp = subset(yourPDGOutFile$pdg,yourPDGOutFile$pdg$year == 2001)
+new2001.10.pdg = subset(tmp, tmp$month == 10) #name based on date for desired spatial output
 # ...  
 
 # Export for GRASS
 setwd("...")
 tmp2 = sprintf("%d:%d:%f", new2001.10.pdg$patchID, new2001.10.pdg$stratumID, new2001.10.pdg$proj_lai)
-write.table(tmp2, file="lai.new.2001.txt", quote=F, row.names=FALSE, col.names=F)
+write.table(tmp2, file="lai.new.2001.txt", quote=F, row.names=FALSE, col.names=F) #name based on date for desired spatial output
+
 
 # To read it back into GRASS:
 # Note: p.rip30.up270.cl = name of my patch map
